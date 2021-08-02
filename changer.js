74e631f1557dcd39db1bf19e5e5bced0ca0523dd
clockln <55965457+clockln@users.noreply.github.com>
2021-08-03 08:46:28 +0900

    Update changer.js

--- changer.js
+++ changer.js
@@ -1,2 +1,16 @@
 var test1 = document.getElementById('test1')
 test1.innerHTML = '<a href="https://www.amazon.co.jp/ref=nav_logo">Amazon</a>'
+
+var buttontest1 = document.getElementById('buttontest1')
+var buttontest2 = document.getElementById('buttontest2')
+
+buttontest1.onclick = ButtonGClick;
+buttontest2.onclick = ButtonAClick;
+
+function ButtonGClick(){
+  test1.innerHTML = '<a href="https://www.google.com/">google</a>'
+}
+
+function ButtonAClick(){
+  test1.innerHTML = '<a href="https://www.amazon.co.jp/ref=nav_logo">Amazon</a>'
+}
