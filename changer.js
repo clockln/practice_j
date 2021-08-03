2d21a59f03de72c838cef9db1e4b1607a1e51a7d
clockln <55965457+clockln@users.noreply.github.com>
2021-08-03 13:38:58 +0900

    Update changer.js

--- changer.js
+++ changer.js
@@ -1,15 +1,27 @@
-var test1 = document.getElementById('test1')
+const test1 = document.getElementById('test1')
 
-var buttontest1 = document.getElementById('buttontest1')
-var buttontest2 = document.getElementById('buttontest2')
+const buttontest1 = document.getElementById('buttontest1')
+const buttontest2 = document.getElementById('buttontest2')
 
 buttontest1.onclick = ButtonGClick;
-buttontest2.onclick = ButtonAClick;
+buttontest2.onclick = tables;
 
 function ButtonGClick(){
   test1.innerHTML = '<a href="https://www.google.com/">google</a>'
 }
 
-function ButtonAClick(){
-  test1.innerHTML = '<a href="https://www.amazon.co.jp/ref=nav_logo">Amazon</a>'
+const table = document.getElementById('table1')
+let aftertable = ''
+let data = [
+  ['AAA',120,2000],
+  ['AQW',852,1010],
+  ['PAHH',14,97541]
+]
+
+function tables(){
+  aftertable = '<tr><th>NAME</th><th>BPM</th><th>NOTES</th></tr>'
+  data.forEach(function (element){
+                 aftertable += '<tr><td>' + element[0] + '</td><td>' + element[1] + '</td><td>' + element[2] + '</td></tr>'
+               });
+  table.innerHTML = aftertable
 }
